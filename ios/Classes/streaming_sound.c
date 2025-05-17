6bd29841c0e2202ba878b89dd565d7754ffe281e
artemnus <@artemnus>
2025-05-17 20:32:33 +0400

    apple devices

--- ios/Classes/streaming_sound.c
+++ ios/Classes/streaming_sound.c
@@ -1,3 +1,6 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../streaming_sound.podspec for more information.
-#include "../../src/streaming_sound.c"
+#include "../../src/miniaudio/miniaudio.c"
+#include "../../src/milo.c"
+#include "../../src/player.c"
+#include "../../src/recorder.c"
